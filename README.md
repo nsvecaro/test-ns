8e272cdd52a4ce00eba61f2e5ad8a89c9c72e1d5
Noviprofil188 <noviprofil188@gmail.com>
2024-12-05 13:54:04 +0100

    Provjera znanja RIWA 5.12.2024.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # test-ns
+
+https://github.com/nsvecaro/test-ns.git
+
+SELECT zaposlenik.id, zaposlenik.ime_prezime
+FROM zaposlenik
+JOIN evidencija
+ON zaposlenik.id=evidencija.zaposlenik
+ORDER BY evidencija.broj_sati DESC
---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page class="flex flex-center">
+   <h1>Evidencija</h1>
+
+   <!-- <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    > -->
+  </q-page>
+</template>
+
+<script setup>
+defineOptions({
+  name: 'IndexPage'
+});
+</script>
